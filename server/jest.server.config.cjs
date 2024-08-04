a95801c75ac2d7131f6f2c5b6e531cf97e5a683e
Glitch (shining-uncovered-onyx) <none>
2024-08-04 22:25:27 +0000

    🐙🥳 Updated with Glitch

--- server/jest.server.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    moduleFileExtensions: ['js'],
-    testMatch: ['**/*.test.js'],
-    transform: {
-        '^.+\\.jsx?$': 'babel-jest',
-    },
-};
